6dc09b3819477ae29223688a9a42cfe3711aaafd
gregs <git@zuluru.org>
2019-07-13 12:53:03 -0400

    Fix fixture to match code

--- tests/Fixture/GroupsFixture.php
+++ tests/Fixture/GroupsFixture.php
@@ -22,44 +22,44 @@ class GroupsFixture extends TestFixture {
 		$this->records = [
 			[
 				'name' 			=> 'Player',
-				'active'		=>	true,
-				'level'			=>	0,
+				'active'		=> true,
+				'level'			=> 0,
 				'description' 	=> 	'You will be participating as a player.'
 			],
 			[
-				'name' 			=> 'Parent',
-				'active'		=>	true,
-				'level'			=>	0,
+				'name' 			=> 'Parent/Guardian',
+				'active'		=> true,
+				'level'			=> 0,
 				'description' 	=> 	'You have one or more children who will be participating as players.'
 			],
 			[
 				'name' 			=> 'Coach',
-				'active'		=>	true,
-				'level'			=>	0,
+				'active'		=> true,
+				'level'			=> 0,
 				'description' 	=> 	'You will be coaching a team that you are not a player on.'
 			],
 			[
 				'name' 			=> 'Volunteer',
-				'active'		=>	true,
-				'level'			=>	1,
+				'active'		=> true,
+				'level'			=> 1,
 				'description' 	=> 	'You plan to volunteer to help organize or run things.'
 			],
 			[
 				'name' 			=> 'Official',
-				'active'		=>	false,
-				'level'			=>	3,
+				'active'		=> false,
+				'level'			=> 3,
 				'description' 	=> 	'You will be acting as an in-game official.'
 			],
 			[
 				'name' 			=> 'Manager',
-				'active'		=>	true,
-				'level'			=>	5,
+				'active'		=> true,
+				'level'			=> 5,
 				'description' 	=> 	'You are an organizational manager with some admin privileges.'
 			],
 			[
 				'name' 			=> 'Administrator',
-				'active'		=>	true,
-				'level'			=>	10,
+				'active'		=> true,
+				'level'			=> 10,
 				'description' 	=> 	'You are an organizational administrator with absolute privileges.'
 			]
 		];
