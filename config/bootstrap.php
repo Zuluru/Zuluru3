f4f8c250ec44866907d79b95b2fdd776ef066ddd
gregs <git@zuluru.org>
2020-04-17 15:39:24 -0400

    New version

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -237,5 +237,5 @@ Configure::write('App.globalListeners', $globalListeners);
 if (!defined('ZULURU_VERSION')) {
 	define('ZULURU_MAJOR', 3);
 	define('ZULURU_MINOR', 2);
-	define('ZULURU_REVISION', 0);
+	define('ZULURU_REVISION', 1);
 }
